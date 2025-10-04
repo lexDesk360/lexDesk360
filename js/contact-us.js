340794c24dc2a519a75e0fff0815cdb9c56c7f01
Wessam Tarek <wessam.tarek@stp.local>
2025-10-04 23:14:24 +0300

    first commit

--- js/contact-us.js
+++ js/contact-us.js
@@ -11,17 +11,30 @@ if (lang == "ar") {
   body.classList.add("rtl");
 }
 
-let translationsData;
-function getData() {
-  fetch("./assets/json/contact-us-page.json")
-    .then((res) => res.json())
-    .then((data) => {
-      translationsData = data;
-      setData()
-    })
-    .catch((err) => console.error(err));
+let translationsData ={
+  "eng": {
+    "pathPage": "Home",
+    "titlePage": "Contact Us",
+    "name": "Name",
+    "emailcon": "Email",
+    "phone": "Phone",
+    "Subject": "Subject",
+    "Message": "Message",
+    "applyNow": "Send Message"
+  },
+  "ar": {
+    "pathPage": " الصفحة الرئيسية ",
+    "titlePage": "تواصل معنا",
+    "name": "الاسم",
+    "emailcon": "الايميل",
+    "phone": "رقم التليفون",
+    "Subject": "الموضوع",
+    "Message": "رسالة",
+    "applyNow": "إرسال"
+  }
 }
-function setData(){
+;
+
   document.querySelectorAll("[data-translate]").forEach((el) => {
   const key = el.getAttribute("data-translate");
   if (el.placeholder !== undefined) {
@@ -30,7 +43,5 @@ function setData(){
     el.innerHTML = translationsData[lang][key] || el.innerHTML;
   }
 });
-}
-getData();
 
 localStorage.setItem("activeForm", null);
