a7ba67290cedebcb721199f5029d75d13784cd08
Wessam Tarek <wessam.tarek@stp.local>
2025-10-28 16:31:02 +0300

    add

---
+++ js/SowaidanAndPartners.js
@@ -0,0 +1,90 @@
+localStorage.setItem("currentPage", "SowaidanAndPartners");
+localStorage.setItem("menuState", "false");
+let lang = localStorage.getItem("lang");
+let body = document.getElementById("body");
+
+if (lang == null && lang != "eng" && lang != "ar") {
+  localStorage.setItem("lang", "eng");
+  lang = "eng";
+}
+if (lang == "ar") {
+  body.classList.add("rtl");
+}
+const item = JSON.parse(localStorage.getItem("selectedNetwork"));
+console.log(item);
+const translations = {
+  eng: {
+    home: "Home",
+    aboutTitle: "About the Firm",
+    services: "Role Within LexDesk360",
+    view: "View Partner Website",
+    parac: "Practice Areas ",
+    sector: "Sector Expertise",
+  },
+  ar: {
+    home: "الصفحة الرئيسية",
+    aboutTitle: "نبذة عن الشركة",
+    services: "الدور داخل LexDesk360 ",
+    view: "زيارة موقع الشريك",
+    parac: "مجالات الممارسة",
+    sector: "الخبرة القطاعية",
+  },
+};
+const translationsData = {
+  pathPage: item.titlePage[lang],
+  titlePage: item.name[lang],
+  about: item.about[lang],
+  within: item.within[lang],
+  practice: item.practice[lang].des,
+  sector: item.sector[lang].des,
+};
+document.getElementById("img1").src = item.image2;
+document.getElementById("img2").src = item.image3;
+
+document.querySelectorAll("[translate-item]").forEach((el) => {
+  const key = el.getAttribute("translate-item");
+  if (el.placeholder !== undefined) {
+    el.placeholder = translationsData[key] || el.placeholder;
+  } else {
+    el.innerText = translationsData[key] || el.innerText;
+  }
+});
+document.querySelectorAll("[data-translate]").forEach((el) => {
+  const key = el.getAttribute("data-translate");
+  if (el.placeholder !== undefined) {
+    el.placeholder = translations[lang][key] || el.placeholder;
+  } else {
+    el.innerText = translations[lang][key] || el.innerText;
+  }
+});
+
+function getlinkedinProfile() {
+  window.open(item.link, "_blank");
+}
+
+let practiceList = document.getElementById("practiceList");
+practiceList.innerHTML = item.practice[lang].content
+  .map(
+    (item, index) => `
+      <p class='bold'>${item.name}</p>
+      <p class='des'>
+      ${item.des}
+      </p>
+    `
+  )
+  .join("");
+
+sectorList.innerHTML = item.sector[lang].content
+  .map(
+    (item, index) => `
+      <p class='bold none'>${item.des}</p>
+     
+    `
+  )
+  .join("");
+console.log(item);
+if (item.link == "") {
+  document.getElementById("linkpage").style.display = "none";
+}
+localStorage.setItem("currentPageNew", 1);
+  localStorage.setItem("activeForm", null);
--- js/network-firms.js
+++ js/network-firms.js
@@ -558,7 +558,7 @@ let networkMidwest = [
     image: "../assets/images/our-network/network1.png",
     image2: "../assets/images/our-network/n1-1.jpg",
     image3: "../assets/images/our-network/n1-2.jpg",
-    url: "./network-details.html",
+    url: "./Sowaidan&Partners.html",
     counrty: {
       eng: "Egypt",
       ar: "مصر",
