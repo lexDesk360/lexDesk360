2b7a8a335e1f775a1be342e25512854152ff5990
Wessam Tarek <wessam.tarek@stp.local>
2026-03-05 13:20:32 +0200

    add

--- js/navbar copy.js
+++ js/navbar copy.js
@@ -1,21 +1,25 @@
+
 let menu = localStorage.getItem("menuState");
 let page = localStorage.getItem("currentPage");
 let language = localStorage.getItem("lang");
 
 const list = document.getElementById("menu");
-if (language == null && language != "eng" && language != "ar") {
+
+// default language
+if (language == null) {
   localStorage.setItem("lang", "eng");
   language = "eng";
 }
 
+// RTL support
+let body = document.getElementById("body");
 if (language == "ar") {
-  let body = document.getElementById("body");
   body.classList.add("rtl");
 } else {
   body.classList.remove("rtl");
 }
 
-
+// translations
 const translationsNav = {
   eng: {
     close: "Close",
@@ -26,54 +30,64 @@ const translationsNav = {
   it: {
     close: "Chiudi",
   },
-  greek: {
+  gr: {
     close: "Κλείσιμο",
   },
-  french: {
+  fr: {
     close: "Fermer",
-  }
+  },
 };
 
+// menu state
 if (menu == "false" || menu == null) {
-  list.innerHTML = `<i onclick="openMenu()" class="bi bi-list cursor"></i>
-            <p onclick="openMenu()" class="cursor"></p>`;
+  list.innerHTML = `
+    <i onclick="openMenu()" class="bi bi-list cursor"></i>
+    <p onclick="openMenu()" class="cursor"></p>
+  `;
 } else if (menu == "true") {
-  list.innerHTML = `<i onclick="closeMenu()" class="bi bi-x cursor"></i>
-                      <p onclick="closeMenu()" class="cursor">${translationsNav[language].close}</p>`;
+  list.innerHTML = `
+    <i onclick="closeMenu()" class="bi bi-x cursor"></i>
+    <p onclick="closeMenu()" class="cursor">
+      ${translationsNav[language].close}
+    </p>
+  `;
 }
 
-function getActiveLang() {
+// set active language in dropdown
+function setActiveLanguage() {
   const languageSelect = document.getElementById("languageSelect");
   if (languageSelect) {
     languageSelect.value = language;
   }
 }
-getActiveLang();
 
+setActiveLanguage();
+
+// open menu
 function openMenu() {
   window.location.href = `./menu`;
   localStorage.setItem("menuState", "true");
 }
 
+// close menu
 function closeMenu() {
   localStorage.setItem("menuState", "false");
   window.location.href = `./${page}`;
 }
 
-
-function openCompanyLinkedin() {
-  window.open("https://www.linkedin.com/company/lexdesk360/", "_blank");
-}
-
+// home
 function goHome() {
   window.location.href = `./home`;
 }
 
+// linkedin
+function openCompanyLinkedin() {
+  window.open("https://www.linkedin.com/company/lexdesk360/", "_blank");
+}
+
+// change language from dropdown
 function changeLanguage() {
   const selectedLang = document.getElementById("languageSelect").value;
   localStorage.setItem("lang", selectedLang);
   window.location.reload();
 }
-
-
-
